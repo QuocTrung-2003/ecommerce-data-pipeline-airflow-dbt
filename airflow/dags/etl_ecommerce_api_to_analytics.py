8eb7e77de88427c30b69d1cc64245c9534586187
Quoc Trung <lenq.trung@gmail.com>
2026-03-31 08:06:19 +0100

    extract_api_data

--- airflow/dags/etl_ecommerce_api_to_analytics.py
+++ airflow/dags/etl_ecommerce_api_to_analytics.py
@@ -1,8 +1,10 @@
 import os, time, requests, logging
+from airflow.models import Variable
 from datetime import datetime, timedelta
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from dateutil import parser
 from airflow import DAG
-from airflow.models import Variable
+
 
 
 API_BASE = os.environ.get("API_BASE_URL", "http://mock-api:8000")
@@ -17,14 +19,16 @@ DEFAULT_ARGS = {
     "retry_delay": timedelta(minutes=2),
     "max_retry_delay":timedelta(minutes=20),
     "retry_exponential_backoff": True
-    
 }
 
-def _get_watermark(name, default_iso):
-    return Variable.get(name, default_var=default_iso)
 
-def _set_watermark(name, iso):
+    # ---------------- INCREMENTAL ----------------
+def _get_watermark(name, default_iso):                      # Take the previous run
+    return Variable.get(name, default_var=default_iso)  
+
+def _set_watermark(name, iso):     #  Save a new run
     Variable.set(name, iso)
+
 
 def _fetch_paged(endpoint, params, max_retries=5):
     page = 1
@@ -71,9 +75,96 @@ def _fetch_paged(endpoint, params, max_retries=5):
         else:
             return
         
-def extract_table(table, endpoint, ts_field, wm_name):
+def extract_api_data(table, endpoint, ts_field, wm_name):
     pg = PostgresHook(postgres_conn_id="postgres_default")
     iso_default = (datetime.utcnow() - timedelta(days=365)).isoformat() + "Z"
+    watermark = _get_watermark(wm_name, iso_default)
+    max_seen = parser.isoparse(watermark)
+    rows = 0
+    for page in _fetch_paged(endpoint, {"updated_since": watermark}):
+        data = page["data"]
+        if not data:
+            continue
+        with pg.get_conn() as conn:
+            with conn.cursor() as cur:
+                if table == "raw_data.customers":
+                    cur.executemany(
+                        """
+                        INSERT INTO raw_data.customers(customer_id,company_name,country,industry,company_size,signup_date,updated_at,is_churned)
+                        VALUES(%(customer_id)s,%(company_name)s,%(country)s,%(industry)s,%(company_size)s,%(signup_date)s,%(updated_at)s,%(is_churned)s)
+                        ON CONFLICT (customer_id) DO UPDATE SET
+                          company_name=EXCLUDED.company_name,
+                          country=EXCLUDED.country,
+                          industry=EXCLUDED.industry,
+                          company_size=EXCLUDED.company_size,
+                          signup_date=EXCLUDED.signup_date,
+                          updated_at=EXCLUDED.updated_at,
+                          is_churned=EXCLUDED.is_churned
+                        """,
+                        data
+                    )
+                elif table == "raw_data.products":
+                    cur.executemany(
+                        """
+                        INSERT INTO raw_data.products(product_id,product_name,category,price,currency,updated_at)
+                        VALUES(%(product_id)s,%(product_name)s,%(category)s,%(price)s,%(currency)s,%(updated_at)s)
+                        ON CONFLICT (product_id) DO UPDATE SET
+                        product_name=EXCLUDED.product_name,
+                        category=EXCLUDED.category,
+                        price=EXCLUDED.price,
+                        currency=EXCLUDED.currency,
+                        updated_at=EXCLUDED.updated_at
+                        """,
+                        data
+                    )
+                elif table == "raw_data.orders":
+                    cur.executemany(
+                        """
+                        INSERT INTO raw_data.orders(order_id,customer_id,total_amount,status,payment_method,country,created_at,updated_at)
+                        VALUES(%(order_id)s,%(customer_id)s,%(total_amount)s,%(status)s,%(payment_method)s,%(country)s,%(created_at)s,%(updated_at)s)
+                        ON CONFLICT (order_id) DO UPDATE SET
+                        customer_id=EXCLUDED.customer_id,
+                        total_amount=EXCLUDED.total_amount,
+                        status=EXCLUDED.status,
+                        payment_method=EXCLUDED.payment_method,
+                        country=EXCLUDED.country,
+                        created_at=EXCLUDED.created_at,
+                        updated_at=EXCLUDED.updated_at
+                        """,
+                        data
+                    )
+                elif table == "raw_data.order_items":
+                    cur.executemany(
+                        """
+                        INSERT INTO raw_data.order_items(order_item_id,order_id,product_id,quantity,price,total_price,created_at,updated_at)
+                        VALUES(%(order_item_id)s,%(order_id)s,%(product_id)s,%(quantity)s,%(price)s,%(total_price)s,%(created_at)s,%(updated_at)s)
+                        ON CONFLICT (order_item_id) DO NOTHING
+                        """,
+                        data
+                    )
+                elif table == "raw_data.visits":
+                    for d in data:
+                        d["bounced"] = int(d.get("bounced", 0))
+                        d["converted"] = int(d.get("converted", 0))
+
+                    cur.executemany(
+                        """
+                        INSERT INTO raw_data.visits(visit_id,customer_id,source,medium,device,country,pageviews,session_duration_s,bounced,converted,visit_start,updated_at)
+                        VALUES(%(visit_id)s,%(customer_id)s,%(source)s,%(medium)s,%(device)s,%(country)s,%(pageviews)s,%(session_duration_s)s,%(bounced)s,%(converted)s,%(visit_start)s,%(updated_at)s)
+                        ON CONFLICT (visit_id) DO NOTHING
+                        """,
+                        data
+                    )
+        rows += len(data)
+        for d in data:
+            ts = parser.isoparse(d.get(ts_field))
+            if ts > max_seen:
+                max_seen = ts
+    _set_watermark(wm_name, max_seen.isoformat())
+    return rows
+
+
+    # ---------------- DAG ----------------
 
 with DAG(
     dag_id="etl_ecommerce_api_to_analytics",
